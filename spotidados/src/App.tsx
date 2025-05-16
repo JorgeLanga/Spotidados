b013d52f8f7cb8f7e46866e15657b6d4552e8483
Jorge Langa <jorgebernardolanga@gmail.com>
2025-05-16 11:04:30 +0200

    Feat: Add Project Structure

---
+++ spotidados/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+export const App=()=> {
+
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+
