d86d1da2ecf162480bb84916c3044290daa55eec
Jorge Langa <jorgebernardolanga@gmail.com>
2025-05-17 21:16:45 +0200

    Update: Update style on register page

--- src/pages/registarPassword.tsx
+++ src/pages/registarPassword.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
-
+import logo from '../imagens/logo.png';
 export const RegistarPassword = () => {
+const inputStyle='bg-white text-black opacity-70% border-1 rounded mx-4 mb-4 py-2 w-70'
   return (
-
-      <body className='bg-'>
-          <form action="">
-        <label htmlFor="">Username</label><br />
-        <input type="text" placeholder='Escreva o seu username' /><br />
-        <label htmlFor="">Password</label><br />
-        <input type="password" placeholder='Escreva a palavra-passe'  /><br />
-         <label htmlFor="">Confirmar</label><br />
-        <input type="password" placeholder='Escreva a mesma palavra-passe'  /><br />
-        <button type='submit'>Enviar</button>
+ <body className='bg-red-950 ' >
+    <div className='flex justify-center items-center h-screen ' >
+      
+      <form action="" className='w-50% h-50%'>
+        <img src={logo} alt="" className="w-80 h-85 p-0 m-0 "/>
+        <input type="text" placeholder='Username' className={inputStyle}/><br /> 
+        <input type="password" placeholder='Palavra-passe'className={inputStyle} /><br />
+        <input type="password" placeholder='Confirmar Palavra-passe' className={inputStyle} /><br />
+        <button type='submit' className=' rounded text-white px-3 py-2 m-4 w-70 bg-red-600  '>Enviar</button>
       </form>
-      </body>
-       
-  
+    </div>
+    
+  </body>
   )
 }
 
